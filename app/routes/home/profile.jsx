5e22a0d082309e7eb8d71f28e9e6d1945d1a15a4
RhaegarTargaryenn <shubhamrana97262@gmail.com>
2024-03-22 13:36:15 +0530

    first commit

---
+++ app/routes/home/profile.jsx
@@ -0,0 +1,95 @@
+import profileImgLarge from '~/assets/profile-large.jpg';
+import profileImgPlaceholder from '~/assets/profile-placeholder.jpg';
+import profileImg from '~/assets/profile.jpg';
+import { Button } from '~/components/button';
+import { DecoderText } from '~/components/decoder-text';
+import { Divider } from '~/components/divider';
+import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { Section } from '~/components/section';
+import { Text } from '~/components/text';
+import { Transition } from '~/components/transition';
+import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
+import styles from './profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    I'm Shubham, a dedicated data scientist residing in Faridabad. Armed with a Master's in Computer Applications and specialized training in data science, I bring a potent blend of academic knowledge and practical skills to the table. Currently seeking exciting opportunities, I aim to apply my expertise in data analysis, machine learning, and predictive analytics to drive impactful outcomes.
+    </Text>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    In my spare time, I devote myself to crafting projects and unwinding with volleyball matches. Whether it's designing captivating dashboards, creating visualizations, or enjoying the adrenaline of volleyball, I'm constantly seeking avenues for creative expression and personal growth. Additionally, I'm passionate about fostering connections and exploring new opportunities. Feel free to reach out—I'm always open to new projects and collaborations.
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {({ visible, nodeRef }) => (
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="https://drive.google.com/file/d/10tjbfhw9BOK37VjzVo0jWFGy7KkGbM4S/view?usp=drive_link"
+                icon="send"
+              >
+                Download Cv
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me smiling like a goofball"
+                />
+                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
